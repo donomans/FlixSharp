aa1a7d95a77805e3b7f42bdf38448e3617fad014
Donovan Sandey <fronn1@gmail.com>
2012-10-16 22:51:17 -0500

    ugh

---
+++ Holders/NetflixPeople.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlixSharp.Holders
+{
+    public class People : IEnumerable<Person>
+    {
+        List<Person> _people = new List<Person>();
+
+        public IEnumerator<Person> GetEnumerator()
+        {
+            return _people.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Queries/NetflixSearch.cs
@@ -0,0 +1,46 @@
+﻿using FlixSharp.Constants;
+using FlixSharp.Holders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FlixSharp.Queries
+{
+    public class NetflixSearch
+    {
+        /// <summary>
+        /// Make a catalog/titles search request
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="max"></param>
+        /// <param name="onuserbehalf">Make the request on the user's behalf if a 
+        /// GetCurrentUserNetflixUserInfo delegate was provided during creation.</param>
+        /// <returns></returns>
+        public Movies SearchTitle(String Title, Int32 Limit = 10, Boolean OnUserBehalf = true,
+            TitleExpansion ExpansionLevel = TitleExpansion.Minimal)
+        {
+            Netflix.Login.CheckInformationSet();
+            return null;
+        }
+
+        public IEnumerable<String> AutoCompleteTitle(String Title, Int32 Limit = 10)
+        {
+            Netflix.Login.CheckInformationSet();
+
+            String url = NetflixConstants.CatalogAutoCompleteUrl + 
+                "?oauth_consumer_key=" + Netflix.Login.ConsumerKey +
+                "&term=" + Title;
+
+            XDocument doc = XDocument.Load(url);
+
+            var titles = from someelement
+                         in doc.Descendants("title")
+                         select someelement.Attribute("short").Value;
+
+            return titles.Take(Limit);
+        }
+    }
+}
