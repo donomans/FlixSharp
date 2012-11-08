df4408121b64239f853decce50bdbbdafdb95bde
Donovan Sandey <fronn1@gmail.com>
2012-11-08 17:16:08 -0600

    some work on throttling - some requests are too easily swamping the netflix API

--- Helpers/AsyncHelpers.cs
+++ Helpers/AsyncHelpers.cs
@@ -9,18 +9,57 @@ using System.Xml.Linq;
 using FlixSharp.Constants;
 using FlixSharp.Holders;
 using FlixSharp.Queries;
+using System.Collections;
+using System.Threading;
+using System.Collections.Concurrent;
 
 namespace FlixSharp.Async
 {
     internal class AsyncHelpers
     {
-        public static async Task<XDocument> LoadXDocumentAsync(String url)
+     
+   
+        ///10 per second seems to be a liberal estimate by Netflix - they are strict....
+        ///seems to trigger failures unless its throttled quite a bit?
+        public static SlowLeak Leak = new SlowLeak(8, 1000); 
+
+        public static async Task<XDocument> ThrottleLoadXDocumentAsync(String url)
         {
             using (WebClient wc = new WebClient())
             {
-                String xml = await wc.DownloadStringTaskAsync(url);
-                return XDocument.Parse(xml);
+                wc.Headers.Add("user-agent", "FilmTrove");
+                try
+                {
+                    Leak.CheckLeak();
+                    String xml = await wc.DownloadStringTaskAsync(url);
+                    return XDocument.Parse(xml);
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Response.Headers["X-Mashery-Error-Code"] == "ERR_403_DEVELOPER_OVER_QPS")
+                    {
+                        Thread.Sleep(150);
+                        throw new NetflixThrottleException(ex);
+                    }
+                    ///possibly throw a throttled exception or something more valid?
+                    throw;
+                }
             }
+        }
+        public static async Task<XDocument> LoadXDocumentAsync(String url)
+        {
+            ///temporary
+            Boolean retry = false;
+            try
+            {
+                return await ThrottleLoadXDocumentAsync(url);
+            }
+            catch (NetflixThrottleException)
+            { retry = true; }
+            if (retry)
+                return await LoadXDocumentAsync(url);
+            else
+                throw new Exception("wat.");
         }
 
         public static async Task<IEnumerable<Title>> GetExpandedMovieDetails(XDocument doc, Boolean OnUserBehalf = true)
@@ -77,6 +116,77 @@ namespace FlixSharp.Async
             foreach (Task<Person> p in people)
                 awaitedpeople.Add(await p);
             return awaitedpeople;
+        }
+    }
+   
+    /// <summary>
+    /// Thanks to Jack Leitch at http://www.pennedobjects.com/2010/10/better-rate-limiting-with-dot-net/ for original code for this
+    /// Modified to simplify for my understanding, but the idea and structure was taken from Jack.
+    /// </summary>
+    internal class SlowLeak
+    {
+        private readonly SemaphoreSlim semaphore;
+        private readonly ConcurrentQueue<Int32> exittimes;
+        private readonly Timer exittimer;
+
+        private Int32 rate = 0;
+        private Int32 waittime = 0;
+
+        public SlowLeak(Int32 Rate, Int32 WaitTimeInMs)
+        {
+            rate = Rate;
+            waittime = WaitTimeInMs;
+
+            semaphore = new SemaphoreSlim(rate, rate);
+            exittimes = new ConcurrentQueue<Int32>();
+            exittimer = new Timer(Leak, null, WaitTimeInMs, -1);
+        }
+
+        private void Leak(object state)
+        {
+            int exitTime;
+            while (exittimes.TryPeek(out exitTime) && unchecked(exitTime - Environment.TickCount) <= 0)
+            {
+                semaphore.Release();
+                exittimes.TryDequeue(out exitTime);
+            }
+
+            int timeUntilNextCheck;
+            if (exittimes.TryPeek(out exitTime))
+                timeUntilNextCheck = unchecked(exitTime - Environment.TickCount);
+            else
+                timeUntilNextCheck = waittime;
+
+            exittimer.Change(timeUntilNextCheck, -1);
+        }
+
+        private Boolean CheckLeak(int millisecondsTimeout)
+        {
+            var entered = semaphore.Wait(millisecondsTimeout);
+            if (entered)
+            {
+                var timeToExit = unchecked(Environment.TickCount + waittime);
+                exittimes.Enqueue(timeToExit);
+            }
+            return entered;
+        }
+
+        public Boolean CheckLeak()
+        {
+            return CheckLeak(Timeout.Infinite);
+        }
+
+        public void Dispose()
+        {
+            semaphore.Dispose();
+            exittimer.Dispose();
+        }
+    }
+
+    public class NetflixThrottleException : WebException
+    {
+        public NetflixThrottleException(WebException ex) : base("ERR_403_DEVELOPER_OVER_QPS", ex)
+        {
         }
     }
 }
